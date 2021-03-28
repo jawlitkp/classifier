08d8af74f95c12a4491a3e9b6cea74b4f3d8f81e
Jawlit Patel <jawlit.patel@gmail.com>
2021-03-28 14:47:07 -0400

    changed roles

--- functions/has_interface_detail.pp
+++ functions/has_interface_detail.pp
@@ -5,28 +5,28 @@
 # them
 #
 # @example checks if an ip exist
-#   has_interface_detail("192.168.88.11")            => true
-#   has_interface_detail("fe80::20c:29ff:fece:f8ec") => true
+#   has_interface_detail('192.168.88.11')            => true
+#   has_interface_detail('fe80::20c:29ff:fece:f8ec') => true
 #
 # @example checks if a network exist
-#   has_interface_detail("192.168.88.0", "network") => true
+#   has_interface_detail('192.168.88.0', 'network') => true
 #
 # @example checks if any interface has a netmask
-#   has_interface_detail("255.255.0.0", "netmask")           => true
-#   has_interface_detail("ffff:ffff:ffff:ffff::", "netmask") => true
+#   has_interface_detail('255.255.0.0', 'netmask')           => true
+#   has_interface_detail('ffff:ffff:ffff:ffff::', 'netmask') => true
 #
 # @param $match [String] an ip or network to match against
-# @param $what [Enum["address", "network", "netmask", "mac"]] what to check
+# @param $what [Enum['address', 'network', 'netmask', 'mac']] what to check
 # @return [Boolean]
 function classifier::has_interface_detail (
   String $match,
-  Enum["address", "network", "netmask", "mac"] $what="address"
+  Enum['address', 'network', 'netmask', 'mac'] $what='address'
 ) {
-  $ips = $facts["networking"]["interfaces"].map |$iname, $interface| {
-    if $what == "mac" {
-      $interface["mac"]
+  $ips = $facts['networking']['interfaces'].map |$iname, $interface| {
+    if $what == 'mac' {
+      $interface['mac']
     } else {
-      ["bindings", "bindings6"].map |$bname| {
+      ['bindings', 'bindings6'].map |$bname| {
         if $bname in $interface {
           $interface[$bname].map |$binding| {
             $binding[$what]
